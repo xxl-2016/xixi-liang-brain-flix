6e7bb28a947b14d1fb78b50da2f80954fe0a0ddb
Xixi Liang <lxx260905@gmail.com>
2024-04-11 19:53:02 -0700

    back to client version

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-require('dotenv').config();
-
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
